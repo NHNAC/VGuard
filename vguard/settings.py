06ef4c1b633e11aeecb0c952b79b39e6736a5980
Usamaalzomor <osama.zomor2010@gmail.com>
2024-02-12 15:15:06 +0200

    feat: Configure PostgreSQL database connection in settings.py description: - Install psycopg2 for proper connection with postgresql - Run migrate to populate the database with the project schema

--- vguard/settings.py
+++ vguard/settings.py
@@ -80,10 +80,14 @@ WSGI_APPLICATION = 'vguard.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'vguard',
+       'USER': 'psqladmin',
+       'PASSWORD': 'psqlpassword',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
