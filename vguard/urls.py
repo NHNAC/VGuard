1fcf35588acb390fbe77782f2bcc81ade0989fde
Usamaalzomor <osama.zomor2010@gmail.com>
2024-02-13 14:29:55 +0200

    refactor: Updated URL configuration and introduce token_obtain_pair url
    - Replaced custom authentication endpoint with TokenObtainPairView for obtaining access and refresh tokens.
    - Removed unused inclusion of core app URLs, now commented out for clarity.
    - Improved readability and consistency of URL configuration in urls.py.

--- vguard/urls.py
+++ vguard/urls.py
@@ -3,11 +3,12 @@ URL configuration for vguard project.
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),  # Endpoint for managing users resources
-    path('', include('core.urls')),  # Customized drf-passwordless-auth urls 
-    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Endpoint for requesting an access token using the refresh token
+    # path('', include('core.urls')),  # Customized drf-passwordless-auth urls
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Endpoint for requesting an access token and refresh token 
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Endpoint for requesting an access token using the refresh token
 ]
