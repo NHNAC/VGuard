409d6706053535f892ff754ce6f8d46d46a4ed7f
Usamaalzomor <osama.zomor2010@gmail.com>
2024-02-22 15:27:57 +0200

    feat: configure serving media files and add requirements.txt

--- vguard/settings.py
+++ vguard/settings.py
@@ -157,3 +157,6 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
--- vguard/urls.py
+++ vguard/urls.py
@@ -4,6 +4,8 @@ URL configuration for vguard project.
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +14,4 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Endpoint for requesting an access token and refresh token 
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Endpoint for requesting an access token using the refresh token
     path('dashboard/', include('dashboard.urls'))  # Endpoint for managing dashboard resources,  
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
